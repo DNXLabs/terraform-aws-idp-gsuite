2d384c5d589c7aa3d13f6287f32b7597b842013b
Allan Denot <adenot@gmail.com>
2020-03-16 16:35:38 +1100

    Audit role for IDP

---
+++ iam-role-audit.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "audit" {
+  name                 = "${var.org_name}-audit"
+  assume_role_policy   = data.aws_iam_policy_document.gsuite.json
+  max_session_duration = var.role_max_session_duration
+}
+
+data "aws_iam_policy_document" "audit_policy" {
+  statement {
+    actions   = ["iam:ListAccountAliases"]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    resources = [
+      "arn:aws:iam::*:role/${var.org_name}-audit",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "audit_assume" {
+  name   = "assume-idp-audit"
+  role   = aws_iam_role.audit.id
+  policy = data.aws_iam_policy_document.audit_policy.json
+}
